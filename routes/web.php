8fb70f86656ebb31fca4b79b5f7d5351b78227ac
suzukiserika <soap-bubble@outlook.com>
2020-08-04 10:51:50 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 以下に追記
+Route::group(['prefix' => 'admin'],
+function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
+
+
+// 課題３
+Route::group(['prefix' => 'admin'],
+function() {
+    Route::get('XXX','Admin\AAAController@bbb');
+});
+
+
