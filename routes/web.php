af1efe53ff88b65d7fbdea600d96334f5f0d83ee
suzukiserika <soap-bubble@outlook.com>
2020-08-11 08:50:46 +0000

    PHP/Laravel 01３課題

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,13 @@ Route::get('XXX','AAAController@bbb');
 
 
 // 以下に追記　(09課題４は21・22行目)
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create'); #追記
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 
