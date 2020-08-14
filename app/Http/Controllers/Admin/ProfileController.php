537422b948783bfdbaf32d89e600f1afb7b80cd7
suzukiserika <soap-bubble@outlook.com>
2020-08-14 07:30:06 +0000

    PHP/Laravel 016課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,16 +16,7 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+
     
     public function create(Request $request)
     {
@@ -33,17 +24,64 @@ class ProfileController extends Controller
         // Varidationを行う
         $this->validate($request, Profile::$rules);
     
-        $news = new Profile;
+        $profiles = new Profile;
         $form = $request->all();
         
         // フォームから送信されてきた_tokenを削除する
         unset($form['_token']);
         
         // データベースに保存する
-        $news->fill($form);
-        $news->save();
+        $profiles->fill($form);
+        $profiles->save();
     
         // admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
     }
+    
+    
+    // 以下を追記
+    public function index(Request $request) #indexメソッド=一覧に表示
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != ''){
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('title',$cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }    
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+    
+    
+    // 以下を追記
+    public function edit(Request $request) 
+    {
+        // Profile Modelからデータを取得する
+        $profiles = Profile::find($request->id);
+        if (empty($profiles)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profiles]);
+    }
+    
+    
+    public function update(Request $request) #updateメソッド=更新
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        
+        // Profile Modelからデータを取得する
+        $profiles = Profile::find($request->id);
+        
+        // 送信されてきたフォームを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        
+        // 該当するデータを上書きして保存する
+        $profiles->fill($profile_form)->save();
+        
+        return redirect('admin/profile/');
+    }
 }
+
